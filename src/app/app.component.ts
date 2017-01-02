aff7d138458be4206cd78c5827b20f08a3ac8761
willierezserve <willie@rezserve.com>
2017-01-02 10:29:18 -0800

    yoyoyo its up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { BOOKS } from './json.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app works!';
+  books = BOOKS;
 }
